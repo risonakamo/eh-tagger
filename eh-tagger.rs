aa15f981d85a22432f1cde35167af35164295640
risonakamo <hmykanae@gmail.com>
2021-04-25 21:43:52 -0400

    update tag api

--- eh-tagger.rs
+++ eh-tagger.rs
@@ -1,12 +1,15 @@
 #![feature(proc_macro_hygiene,decl_macro)]
 #![allow(non_snake_case)]
 
-use rocket::get;
+use rocket::{get,post};
 use rocket_contrib::serve::StaticFiles;
-use rocket_contrib::json::JsonValue;
+use rocket_contrib::json::{JsonValue,Json};
 use rocket_contrib::json;
 
 use eh_tagger::tag_entry::getTagEntrys;
+use eh_tagger::datafile::tag_file::updateTagFile;
+
+use eh_tagger::types::tag_types::TagUpdate;
 
 /// get tag entries api. retrieves tag entries.
 #[get("/get-tag-entries")]
@@ -20,10 +23,19 @@ fn api_getTagEntries()->JsonValue
     ))
 }
 
+/// update the tag file.
+#[post("/update-tag",format="json",data="<request>")]
+fn api_updateTag(request:Json<TagUpdate>)->JsonValue
+{
+    let tagupdate:TagUpdate=request.into_inner();
+    updateTagFile("data/testdata.json",tagupdate);
+    return json!("complete");
+}
+
 fn main()
 {
     rocket::ignite()
         .mount("/",StaticFiles::from("web"))
-        .mount("/",rocket::routes!(api_getTagEntries))
+        .mount("/",rocket::routes![api_getTagEntries,api_updateTag])
         .launch();
 }
